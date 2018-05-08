41d62e016b4311f3bc0caa9feb826c2ce75b6760
NhutKhanhng <nhutkhanhng@gmail.com>
2018-05-08 18:11:13 +0700

    "init again from Res GameDesign"

---
+++ 3rd Person Cameras/Assets/Scripts/PlayerData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "ScriptableObject/PlayerData")]
+public class PlayerData : SingletonScriptableOBject<PlayerData>
+{
+    public int maximumPlayerHealth;
+    public int startingLives;
+}
